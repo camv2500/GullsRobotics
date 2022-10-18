d8aae789519262c02b3b10ab9aa9a4b0a0f91323
isaac <isaacd4444@gmail.com>
2022-10-18 16:40:50 -0400

    created moveAll and exitAuton Functions

--- Odometry/Dylan62Program-2022-10-18T13-26-06/src/function.h
+++ Odometry/Dylan62Program-2022-10-18T13-26-06/src/function.h
@@ -1,1 +1,17 @@
-//Isaac will work on this later today
+
+
+void moveAll(double power){
+    leftMotor1.spin(forward,power,pct);
+    rightMotor1.spin(forward,power,pct);
+
+}
+
+void exitAuton(){
+    Brain.Screen.print(error);
+    Brain.Screen.print(" ");
+    Brain.Screen.print(derivative);
+    Brain.Screen.newLine();
+    Brain.Screen.print("Leaving Auton");
+    leftMotor1.spin(forward,0,pct);
+    rightMotor1.spin(forward,0,pct);
+}
