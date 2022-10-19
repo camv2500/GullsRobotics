93a67f04a5645d858512d3d4c348bfa1799216d0
Dylan Williams <dylan.williams.mmhs@gmail.com>
2022-10-19 12:29:56 -0400

    Bug fixes using GitHub
    Still getting used to working with GitHub

---
+++ Odometry/Dylan62Program-2022-10-18T13-26-06/include/function.h
@@ -0,0 +1,21 @@
+#include "robot-config.h"
+
+void moveAll(double power){
+    leftMotor1.spin(forward,power,pct);
+    //leftMotor2.spin(forward,power,pct);
+    //leftMotor3.spin(forward,power,pct);
+    rightMotor1.spin(forward,power,pct);
+    //rightMotor2.spin(forward,power,pct);
+    //rightMotor3.spin(forward,power,pct);
+
+}
+
+void exitAuton(){
+    //Brain.Screen.print(error);
+    Brain.Screen.print(" ");
+    //Brain.Screen.print(derivative);
+    Brain.Screen.newLine();
+    Brain.Screen.print("Leaving Auton");
+    leftMotor1.spin(forward,0,pct);
+    rightMotor1.spin(forward,0,pct);
+}
