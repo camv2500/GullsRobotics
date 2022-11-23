4b734bd6d2d218bbd4f7f8c231f19117366050f9
Dylan Williams <dylan.williams.mmhs@gmail.com>
2022-11-23 17:10:09 -0500

    MotionPD

---
+++ Motion/include/motion.h
@@ -0,0 +1,20 @@
+#include "robot-config.h"
+#include "vex.h"
+
+void PDLoop(double setPoint) {
+  double error = setPoint;
+  double kP = 0, kI = 0, kD = 0, prevError = 0, integral = 0, derivative = 0, power = 0;
+  double sensorValue = 0;
+
+  while (fabs(error) > 1) {
+    sensorValue = LeftMotor1.position(rev) + RightMotor1.position(rev) / 2;
+    error = setPoint - sensorValue;
+    integral = integral + error;
+    if (error <= 0 || error > (setPoint / 8)) {
+      integral = 0;
+    }
+    derivative = error - prevError;
+    prevError = error;
+    power = error * kP + integral * kI + derivative * kD;
+  }
+}
