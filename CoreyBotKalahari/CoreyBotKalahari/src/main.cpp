7163cc5ab78272809b651b8b7281429c38b127c4
Corey Phillips <cep.personal@protonmail.com>
2023-01-19 07:01:23 -0500

    updates to controls

--- CoreyBotKalahari/CoreyBotKalahari/src/main.cpp
+++ CoreyBotKalahari/CoreyBotKalahari/src/main.cpp
@@ -10,21 +10,21 @@
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
-// dL1                  motor         20              
-// dL2                  motor         19              
+// dL1                  motor         13              
+// dL2                  motor         14              
 // dL3                  motor         15              
-// dL4                  motor         17              
-// dR1                  motor         12              
-// dR2                  motor         11              
-// dR3                  motor         13              
-// dR4                  motor         14              
-// rlr                  motor         16              
+// dL4                  motor         16              
+// dR1                  motor         17              
+// dR2                  motor         18              
+// dR3                  motor         19              
+// dR4                  motor         20              
+// rlr                  motor         2               
 // Controller1          controller                    
 // mag                  digital_out   A               
-// fw1                  motor         1               
-// fw2                  motor         2               
+// fw1                  motor         11              
+// fw2                  motor         12              
 // indx                 digital_out   B               
-// itk                  motor         3               
+// itk                  motor         1               
 // endG                 digital_out   C               
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
@@ -104,19 +104,20 @@ void usercontrol(void) {
     Y: Endgame deploy*/
 
     //Drive motor control---------------------------------------------
-    dL1.spin(fwd, Controller1.Axis3.value(), vex::velocityUnits::pct);
-    dL2.spin(fwd, Controller1.Axis3.value(), vex::velocityUnits::pct);
-    dL3.spin(fwd, Controller1.Axis3.value(), vex::velocityUnits::pct);
-    dL4.spin(fwd, Controller1.Axis3.value(), vex::velocityUnits::pct);
-    dR1.spin(fwd, Controller1.Axis2.value(), vex::velocityUnits::pct);
-    dR2.spin(fwd, Controller1.Axis2.value(), vex::velocityUnits::pct);
-    dR3.spin(fwd, Controller1.Axis2.value(), vex::velocityUnits::pct);
-    dR4.spin(fwd, Controller1.Axis2.value(), vex::velocityUnits::pct);
+    
+    dL1.spin(fwd, Controller1.Axis2.value(), vex::velocityUnits::pct);
+    dL2.spin(fwd, Controller1.Axis2.value(), vex::velocityUnits::pct);
+    dL3.spin(fwd, Controller1.Axis2.value(), vex::velocityUnits::pct);
+    dL4.spin(fwd, Controller1.Axis2.value(), vex::velocityUnits::pct);
+    dR1.spin(fwd, Controller1.Axis3.value(), vex::velocityUnits::pct);
+    dR2.spin(fwd, Controller1.Axis3.value(), vex::velocityUnits::pct);
+    dR3.spin(fwd, Controller1.Axis3.value(), vex::velocityUnits::pct);
+    dR4.spin(fwd, Controller1.Axis3.value(), vex::velocityUnits::pct);
     //----------------------------------------------------------------
 
     //Intake Control-----------------------------
     if(Controller1.ButtonL2.pressing()){
-      mag.set(false);
+      mag.set(true);
       indx.set(false);
       itk.spin(fwd, 100, pct);
     }else if(Controller1.ButtonRight.pressing()){
@@ -133,19 +134,31 @@ void usercontrol(void) {
 
     //Flywheel Control-----------------------------------------------------------
     if(Controller1.ButtonR1.pressing()){
-      mag.set(true);
-      fw1.spin(fwd, 100, pct);
-      fw2.spin(fwd, 100, pct);
+      mag.set(false);
+      fw1.spin(reverse, 100, pct);
+      fw2.spin(reverse, 100, pct);
+      if(Controller1.ButtonR2.pressing())
+      {
+        fw1.spin(reverse, 100, pct);
+        fw2.spin(reverse, 100, pct);
+        indx.set(true);
+        vex::task::sleep(500);
+        indx.set(false);
+        vex::task::sleep(500);
+      }
     }else if(Controller1.ButtonR1.pressing() && Controller1.ButtonR2.pressing()){
       indx.set(true);
+      vex::task::sleep(1000);
       indx.set(false);
+      wait(1000, msec);
       indx.set(true);
+      wait(1000, msec);
       indx.set(false);
+      wait(1000, msec);
       indx.set(true);
+      wait(1000, msec);
       indx.set(false);
     }else{
-      mag.set(false);
-      indx.set(false);
       fw1.stop(brakeType::coast);
       fw2.stop(brakeType::coast);
     }
@@ -156,7 +169,7 @@ void usercontrol(void) {
       endG.set(true);
     }
     //-----------------------------------
-
+    
     wait(20, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
   }
--- CoreyBotKalahari/CoreyBotKalahari/src/robot-config.cpp
+++ CoreyBotKalahari/CoreyBotKalahari/src/robot-config.cpp
@@ -8,21 +8,21 @@ using code = vision::code;
 brain  Brain;
 
 // VEXcode device constructors
-motor dL1 = motor(PORT20, ratio6_1, true);
-motor dL2 = motor(PORT19, ratio6_1, true);
-motor dL3 = motor(PORT15, ratio6_1, true);
-motor dL4 = motor(PORT17, ratio6_1, true);
-motor dR1 = motor(PORT12, ratio6_1, false);
-motor dR2 = motor(PORT11, ratio6_1, false);
-motor dR3 = motor(PORT13, ratio6_1, false);
-motor dR4 = motor(PORT14, ratio6_1, false);
-motor rlr = motor(PORT16, ratio36_1, false);
+motor dL1 = motor(PORT13, ratio6_1, false);
+motor dL2 = motor(PORT14, ratio6_1, false);
+motor dL3 = motor(PORT15, ratio6_1, false);
+motor dL4 = motor(PORT16, ratio6_1, false);
+motor dR1 = motor(PORT17, ratio6_1, true);
+motor dR2 = motor(PORT18, ratio6_1, true);
+motor dR3 = motor(PORT19, ratio6_1, true);
+motor dR4 = motor(PORT20, ratio6_1, true);
+motor rlr = motor(PORT2, ratio36_1, false);
 controller Controller1 = controller(primary);
 digital_out mag = digital_out(Brain.ThreeWirePort.A);
-motor fw1 = motor(PORT1, ratio6_1, true);
-motor fw2 = motor(PORT2, ratio6_1, false);
+motor fw1 = motor(PORT11, ratio6_1, true);
+motor fw2 = motor(PORT12, ratio6_1, false);
 digital_out indx = digital_out(Brain.ThreeWirePort.B);
-motor itk = motor(PORT3, ratio6_1, false);
+motor itk = motor(PORT1, ratio6_1, false);
 digital_out endG = digital_out(Brain.ThreeWirePort.C);
 
 // VEXcode generated functions
