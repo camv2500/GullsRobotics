4326e2dde769aff85c319d4770d7115d28dc6ebe
Dylan Williams <dylan.williams.mmhs@gmail.com>
2023-01-30 13:04:14 -0500

    Cleaning Code

--- Motion/include/motion.h
+++ Motion/include/motion.h
@@ -1,68 +1,6 @@
 #include "robot-config.h"
 #include "vex.h"
 
-void PIDLoop(double setPoint, bool isTurning = false, bool usingFlyWheel = false) {
-  //setpoint is in number of degrees required
-  double error = setPoint;
-  double kP = 0.28, kI = 0.01, kD = 0.25, prevError = 0, integral = 0, derivative = 0, power = 0;
-  double sensorValue = 0;
-  double kF, feedForward;
-  bool firstTime = true;
-
-  FlyWheel1.setPosition(0,degrees);
-  FlyWheel2.setPosition(0,degrees);
-
-  /*while (fabs(error) > 0.4 && !usingFlyWheel) {
-    if (isTurning) {sensorValue = RightMotor1.position(degrees);}
-    else {sensorValue = (LeftMotor1.position(degrees) + RightMotor1.position(degrees)) / 2;}
-    error = setPoint - sensorValue;
-
-    integral = integral + error;
-    if (fabs(integral) > 500) {integral = 500;}
-
-    derivative = error - prevError;
-    prevError = error;
-
-    power = error * kP + integral * kI + derivative * kD;
-
-    if (isTurning) {
-      LeftMotor1.spin(reverse, power, pct);
-      RightMotor1.spin(forward, power, pct);
-    }
-    else {
-      LeftMotor1.spin(forward, power, pct);
-      RightMotor1.spin(forward, power, pct);
-    }
-
-    wait(20, msec);
-  }*/
-
-  while (usingFlyWheel) {
-    if (firstTime) {
-    firstTime = false;
-    feedForward = setPoint;
-    kP = 0.0018;
-    kI = 0;
-    kD = 0.000018;
-    kF = 0.0213;}
-
-    error = setPoint - (FlyWheel1.velocity(rpm) + FlyWheel2.velocity(rpm) / 2);
-
-    integral = integral + error;
-    if (fabs(integral) > 12000) {integral = 12000;}
-
-    derivative = error - prevError;
-    prevError = error;
-
-    power = error * kP + integral * kI + derivative * kD + feedForward * kF;
-
-    FlyWheel1.spin(forward, power, volt);
-    FlyWheel2.spin(forward, power, volt);
-
-    wait(10, msec);
-  }
-}
-
 //current function for turning the robot, converts the degrees given to revolutions required and sends it to the PIDLoop
 /*void turnRobot(double setDegrees) {
   //need to convert to using radians to decrease the amount of math
@@ -81,3 +19,227 @@ void PIDLoop(double setPoint, bool isTurning = false, bool usingFlyWheel = false
 
   PIDLoop(requiredRevolutions, true);
 }*/
+
+/*
+static bool pidRunning = true, isTurning = false;
+static bool resetEncoders = false;
+static float pidSetDegrees = 0;
+
+double kP = 0.088, kI = 0.0, kD = 0.06;
+double error = 0, prevError = 0, integral = 0, derivative = 0;
+double power = 0, sensorValue = 0, lSensor = 0, rSensor = 0;
+
+int pidController() {
+  while (pidRunning) {
+    if (resetEncoders) {
+      resetEncoders = false;
+      lMotor11.setPosition(0, degrees);
+      lMotor12.setPosition(0, degrees);
+      lMotor13.setPosition(0, degrees);
+      lMotor14.setPosition(0, degrees);
+      rMotor17.setPosition(0, degrees);
+      rMotor18.setPosition(0, degrees);
+      rMotor19.setPosition(0, degrees);
+      rMotor20.setPosition(0, degrees);
+      integral = 0;
+      derivative = 0;
+    }
+
+    if (pidSetDegrees != 0) {
+      lSensor = (lMotor11.position(degrees) + lMotor12.position(degrees) + 
+        lMotor13.position(degrees) + lMotor14.position(degrees)) / 4;
+      rSensor = (rMotor17.position(degrees) + rMotor18.position(degrees) + 
+        rMotor19.position(degrees) + rMotor20.position(degrees)) / 4;
+      if (isTurning) {sensorValue = rSensor;}
+      else {sensorValue = (lSensor + rSensor) / 2;}
+      error = pidSetDegrees - sensorValue;
+
+      integral = integral + error;
+      if (fabs(integral) > 5000) {integral = 500;}
+
+      derivative = error - prevError;
+      prevError = error;
+
+      power = error * kP + integral * kI + derivative * kD;
+
+      if (isTurning) {
+        lMotor11.spin(reverse, power, pct); rMotor17.spin(forward, power, pct);
+        lMotor12.spin(reverse, power, pct); rMotor18.spin(forward, power, pct);
+        lMotor13.spin(reverse, power, pct); rMotor19.spin(forward, power, pct);
+        lMotor14.spin(reverse, power, pct); rMotor20.spin(forward, power, pct);
+      }
+      else {
+        lMotor11.spin(forward, power, pct); rMotor17.spin(forward, power, pct);
+        lMotor12.spin(forward, power, pct); rMotor18.spin(forward, power, pct);
+        lMotor13.spin(forward, power, pct); rMotor19.spin(forward, power, pct);
+        lMotor14.spin(forward, power, pct); rMotor20.spin(forward, power, pct);
+      }
+    }
+    else {
+      lMotor11.spin(forward, 0, pct);
+      rMotor17.spin(forward, 0, pct);
+      lMotor12.spin(forward, 0, pct);
+      rMotor18.spin(forward, 0, pct);
+      lMotor13.spin(forward, 0, pct);
+      rMotor19.spin(forward, 0, pct);
+      lMotor14.spin(forward, 0, pct);
+      rMotor20.spin(forward, 0, pct);
+    }
+
+    wait(20, msec);
+  }
+
+  return 1;
+}
+
+static bool resetFlywheelEncoders = false;
+static float flywheelSetRPM = 0;
+double fkP = 0.0018, fkI = 0, fkD = 0.00018, fkF = 0.0212;
+double flyWheelError = 0, fprevError = 0, fintegral = 0, fderivative = 0, fpower = 0;
+double kF, feedForward;
+
+int flyWheelController() {
+  while (1) {
+    if (resetFlywheelEncoders) {
+      resetFlywheelEncoders = false;
+      fintegral = 0;
+      fderivative = 0;
+      feedForward = flywheelSetRPM; 
+    }
+
+    if (flywheelSetRPM != 0) {
+      flyWheelError = flywheelSetRPM - (FlyWheel1.velocity(rpm) + FlyWheel2.velocity(rpm) / 2);
+
+      fintegral = fintegral + flyWheelError;
+      if (fabs(fintegral) > 12000) {fintegral = 12000;}
+
+      fderivative = flyWheelError - fprevError;
+      fprevError = flyWheelError;
+
+      fpower = flyWheelError * fkP + fintegral * fkI + fderivative * fkD + feedForward * fkF;
+
+      FlyWheel1.spin(forward, fpower, volt);
+      FlyWheel2.spin(forward, fpower, volt);
+
+      wait(20, msec);
+    }
+    else {
+      FlyWheel1.spin(forward, 0, volt);
+      FlyWheel2.spin(forward, 0, volt);
+    }
+  }
+
+  return 1;
+}
+
+void turnRobot(double setDegrees) {
+  double turnDiameter = 12.0;
+
+  double requiredInches = setDegrees / 360 * M_PI * turnDiameter;
+
+  //convert the inches to revolutions
+  double circumferenceOfWheel = 3.5 * M_PI;
+  double outputRat = 3.0/5.0;
+  double requiredRevolutions = (requiredInches / circumferenceOfWheel) * outputRat * 360.0;
+
+  isTurning = true;
+  resetEncoders = true;
+  pidSetDegrees = requiredRevolutions;
+}
+*/
+
+void SpinMotors(double power, bool isTurning = false) {
+  if(isTurning) {
+    lMotor11.spin(reverse, power, pct); rMotor17.spin(forward, power, pct);
+    lMotor12.spin(reverse, power, pct); rMotor18.spin(forward, power, pct);
+    lMotor13.spin(reverse, power, pct); rMotor19.spin(forward, power, pct);
+    lMotor14.spin(reverse, power, pct); rMotor20.spin(forward, power, pct);
+  }
+  else {
+    lMotor11.spin(forward, power, pct); rMotor17.spin(forward, power, pct);
+    lMotor12.spin(forward, power, pct); rMotor18.spin(forward, power, pct);
+    lMotor13.spin(forward, power, pct); rMotor19.spin(forward, power, pct);
+    lMotor14.spin(forward, power, pct); rMotor20.spin(forward, power, pct);
+  }
+}
+
+double kP = 0.088, kI = 0.0, kD = 0.06;
+double error = 0, prevError = 0, integral = 0, derivative = 0;
+double power = 0, sensorValue = 0, lSensor = 0, rSensor = 0;
+
+//the distance is in revolutions, the encoders should only be reset on first use
+void runPID(double pidSetDegrees, bool resetEncoders = false, bool isTurning = false) {
+  if (resetEncoders) {
+    resetEncoders = false;
+    lMotor11.setPosition(0, degrees); lMotor12.setPosition(0, degrees); lMotor13.setPosition(0, degrees); lMotor14.setPosition(0, degrees);
+    rMotor17.setPosition(0, degrees); rMotor18.setPosition(0, degrees); rMotor19.setPosition(0, degrees); rMotor20.setPosition(0, degrees);
+    integral = 0;
+    derivative = 0;
+  }
+
+  lSensor = (lMotor11.position(degrees) + lMotor12.position(degrees) + 
+    lMotor13.position(degrees) + lMotor14.position(degrees)) / 4;
+  rSensor = (rMotor17.position(degrees) + rMotor18.position(degrees) + 
+    rMotor19.position(degrees) + rMotor20.position(degrees)) / 4;
+  if (isTurning) {sensorValue = rSensor;}
+  else {sensorValue = (lSensor + rSensor) / 2;}
+  error = pidSetDegrees - sensorValue;
+
+  integral = integral + error;
+  if (fabs(integral) > 5000) {integral = 500;}
+
+  derivative = error - prevError;
+  prevError = error;
+
+  power = error * kP + integral * kI + derivative * kD;
+  
+  if (isTurning) {SpinMotors(power, true);}
+  else {SpinMotors(power);}
+}
+
+double fkP = 0.0018, fkI = 0, fkD = 0.00018, fkF = 0.0212;
+double flyWheelError = 0, fprevError = 0, fintegral = 0, fderivative = 0, fpower = 0;
+double kF, feedForward;
+
+void runFlywheel(double flywheelSetRPM = 0, bool resetFlywheelEncoders = false) {
+  if (resetFlywheelEncoders) {
+    resetFlywheelEncoders = false;
+    fintegral = 0;
+    fderivative = 0;
+    feedForward = flywheelSetRPM; 
+  }
+
+  if (flywheelSetRPM != 0) {
+    flyWheelError = flywheelSetRPM - (FlyWheel1.velocity(rpm) + FlyWheel2.velocity(rpm) / 2);
+
+    fintegral = fintegral + flyWheelError;
+    if (fabs(fintegral) > 12000) {fintegral = 12000;}
+
+    fderivative = flyWheelError - fprevError;
+    fprevError = flyWheelError;
+
+    fpower = flyWheelError * fkP + fintegral * fkI + fderivative * fkD + feedForward * fkF;
+
+    FlyWheel1.spin(forward, fpower, volt);
+    FlyWheel2.spin(forward, fpower, volt);
+  }
+  else {
+    FlyWheel1.spin(forward, 0, volt);
+    FlyWheel2.spin(forward, 0, volt);
+  }
+}
+
+//convert degrees to inches
+double ConvertDegreesToInches(double setDegrees) {
+  double turnDiameter = 12.0;
+  double requiredInches = setDegrees / 360 * M_PI * turnDiameter;
+  return requiredInches;
+}
+
+//convert the inches to revolutions
+double ConvertInchesToRevolutions(double requiredInches) {
+  double circumferenceOfWheel = 3.5 * M_PI;
+  double outputRat = 3.0/5.0;
+  double requiredRevolutions = (requiredInches / circumferenceOfWheel) * outputRat * 360.0;
+  return requiredRevolutions;
+}
--- Motion/src/main.cpp
+++ Motion/src/main.cpp
@@ -65,133 +65,57 @@ void pre_auton(void) {
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
-static bool pidRunning = true, isTurning = false;
-static bool resetEncoders = false;
-static float pidSetDegrees = 0;
+static bool isAuton = false, isPID = false, isTurning = false, isFlywheel = false; 
+static bool resetPID = true, resetTurning = true, resetFlywheel = true;
+static double setPID = 0, setTurning = 0, setFlywheel = 0;
 
-double kP = 0.088, kI = 0.0, kD = 0.06;
-double error = 0, prevError = 0, integral = 0, derivative = 0;
-double power = 0, sensorValue = 0, lSensor = 0, rSensor = 0;
-
-int pidController() {
-  while (pidRunning) {
-    if (resetEncoders) {
-      resetEncoders = false;
-      lMotor11.setPosition(0, degrees);
-      lMotor12.setPosition(0, degrees);
-      lMotor13.setPosition(0, degrees);
-      lMotor14.setPosition(0, degrees);
-      rMotor17.setPosition(0, degrees);
-      rMotor18.setPosition(0, degrees);
-      rMotor19.setPosition(0, degrees);
-      rMotor20.setPosition(0, degrees);
-      integral = 0;
-      derivative = 0;
-    }
-
-    if (pidSetDegrees != 0) {
-      lSensor = (lMotor11.position(degrees) + lMotor12.position(degrees) + 
-        lMotor13.position(degrees) + lMotor14.position(degrees)) / 4;
-      rSensor = (rMotor17.position(degrees) + rMotor18.position(degrees) + 
-        rMotor19.position(degrees) + rMotor20.position(degrees)) / 4;
-      if (isTurning) {sensorValue = rSensor;}
-      else {sensorValue = (lSensor + rSensor) / 2;}
-      error = pidSetDegrees - sensorValue;
-
-      integral = integral + error;
-      if (fabs(integral) > 5000) {integral = 500;}
-
-      derivative = error - prevError;
-      prevError = error;
-
-      power = error * kP + integral * kI + derivative * kD;
-
-      if (isTurning) {
-        lMotor11.spin(reverse, power, pct); rMotor17.spin(forward, power, pct);
-        lMotor12.spin(reverse, power, pct); rMotor18.spin(forward, power, pct);
-        lMotor13.spin(reverse, power, pct); rMotor19.spin(forward, power, pct);
-        lMotor14.spin(reverse, power, pct); rMotor20.spin(forward, power, pct);
+int autonController() {
+  while(isAuton) {
+    if (isPID) {
+      if (resetPID) {
+        setPID = ConvertInchesToRevolutions(setPID);
+        runPID(setPID, true);
+        resetPID = false;
       }
-      else {
-        lMotor11.spin(forward, power, pct); rMotor17.spin(forward, power, pct);
-        lMotor12.spin(forward, power, pct); rMotor18.spin(forward, power, pct);
-        lMotor13.spin(forward, power, pct); rMotor19.spin(forward, power, pct);
-        lMotor14.spin(forward, power, pct); rMotor20.spin(forward, power, pct);
+      else {runPID(setPID);}
+    }
+
+    if (isTurning) {
+      if (resetTurning) {
+        setTurning = ConvertDegreesToInches(setTurning);
+        setTurning = ConvertInchesToRevolutions(setTurning);
+        runPID(setTurning, true, true);
+        resetTurning = false;
       }
-    }
-    else {
-      lMotor11.spin(forward, 0, pct);
-      rMotor17.spin(forward, 0, pct);
-      lMotor12.spin(forward, 0, pct);
-      rMotor18.spin(forward, 0, pct);
-      lMotor13.spin(forward, 0, pct);
-      rMotor19.spin(forward, 0, pct);
-      lMotor14.spin(forward, 0, pct);
-      rMotor20.spin(forward, 0, pct);
+      else {runPID(setTurning, false, true);}
     }
 
-    wait(20, msec);
+    if (isFlywheel) {
+      if (resetFlywheel) {
+        runFlywheel(setFlywheel, true);
+        resetFlywheel = false;
+      }
+      else {runFlywheel(setFlywheel);}
+    }
+
+    wait(10, msec);
   }
-
   return 1;
-}
-
-static bool resetFlywheelEncoders = false;
-static float flywheelSetRPM = 0;
-double fkP = 0.0018, fkI = 0, fkD = 0.00018, fkF = 0.0212;
-double flyWheelError = 0, fprevError = 0, fintegral = 0, fderivative = 0, fpower = 0;
-double kF, feedForward;
-
-int flyWheelController() {
-  while (1) {
-    if (resetFlywheelEncoders) {
-      resetFlywheelEncoders = false;
-      fintegral = 0;
-      fderivative = 0;
-      feedForward = flywheelSetRPM; 
-    }
-
-    if (flywheelSetRPM != 0) {
-      flyWheelError = flywheelSetRPM - (FlyWheel1.velocity(rpm) + FlyWheel2.velocity(rpm) / 2);
-
-      fintegral = fintegral + flyWheelError;
-      if (fabs(fintegral) > 12000) {fintegral = 12000;}
-
-      fderivative = flyWheelError - fprevError;
-      fprevError = flyWheelError;
-
-      fpower = flyWheelError * fkP + fintegral * fkI + fderivative * fkD + feedForward * fkF;
-
-      FlyWheel1.spin(forward, fpower, volt);
-      FlyWheel2.spin(forward, fpower, volt);
-
-      wait(20, msec);
-    }
-    else {
-      FlyWheel1.spin(forward, 0, volt);
-      FlyWheel2.spin(forward, 0, volt);
-    }
-  }
-
-  return 1;
-}
-
-void turnRobot(double setDegrees) {
-  double turnDiameter = 12.0;
-
-  double requiredInches = setDegrees / 360 * M_PI * turnDiameter;
-
-  //convert the inches to revolutions
-  double circumferenceOfWheel = 3.5 * M_PI;
-  double outputRat = 3.0/5.0;
-  double requiredRevolutions = (requiredInches / circumferenceOfWheel) * outputRat * 360.0;
-
-  isTurning = true;
-  resetEncoders = true;
-  pidSetDegrees = requiredRevolutions;
 }
 
 void autonomous(void) {
+  isAuton = true;
+  task StartAuton(autonController);
+
+  setFlywheel = 400;
+  isFlywheel = true;
+
+  setPID = 12;
+  isPID = true;
+  wait(1000, msec);
+  isPID = false;
+  
+  /*
   task StartPID(pidController);
   task StartFlyWheel(flyWheelController);
   double circumferenceOfWheel = 3.5 * M_PI;
@@ -227,53 +151,9 @@ void autonomous(void) {
   diskPusher1.set(false);
   vex::task::sleep(1000);
   diskPusher1.set(true);
-
-/*
-  distance = 54;
-  convDistance = (distance/circumferenceOfWheel)*outputRat*360.0;
-  resetEncoders = true;
-  pidSetDegrees = convDistance;
-  vex::task::sleep(2500);
-  pidSetDegrees = 0;
-
-  turnRobot(-98);
-  vex::task::sleep(2000);
-  isTurning = false;
-  pidSetDegrees = 0;
-
-  distance = 42;
-  convDistance = (distance/circumferenceOfWheel)*outputRat*360.0;
-  resetEncoders = true;
-  pidSetDegrees = convDistance;
-  vex::task::sleep(2500);
-  pidSetDegrees = 0;
-
-  turnRobot(-50);
-  vex::task::sleep(2000);
-  isTurning = false;
-  pidSetDegrees = 0;
-
-  distance = 5;
-  convDistance = (distance/circumferenceOfWheel)*outputRat*360.0;
-  resetEncoders = true;
-  pidSetDegrees = convDistance;
-  resetFlywheelEncoders = true;
-  flywheelSetRPM = 500;
-  vex::task::sleep(2500);
-  pidSetDegrees = 0;
-*/
-  Brain.Screen.print("isExited");
-  /*
-  lMotor11.spin(fwd, 0, vex::velocityUnits::pct);
-  rMotor17.spin(fwd, 0, vex::velocityUnits::pct);
-  lMotor12.spin(fwd, 0, vex::velocityUnits::pct);
-  rMotor18.spin(fwd, 0, vex::velocityUnits::pct);
-  lMotor13.spin(fwd, 0, vex::velocityUnits::pct);
-  rMotor19.spin(fwd, 0, vex::velocityUnits::pct);
-  lMotor14.spin(fwd, 0, vex::velocityUnits::pct);
-  rMotor20.spin(fwd, 0, vex::velocityUnits::pct);
   */
-  
+  Brain.Screen.print("isExited");
+  SpinMotors(0);
 }
 
 
@@ -287,6 +167,7 @@ void autonomous(void) {
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
+/*
 void toggleFlyWheelOn() {
   resetFlywheelEncoders = true;
   flywheelSetRPM = 462;
@@ -295,9 +176,10 @@ void toggleFlyWheelOn() {
 void toggleFlyWheelOff() {
   flywheelSetRPM = 0;
 }
+*/
 
 void usercontrol(void) {
-  task StartFlyWheel(flyWheelController);
+  //task StartFlyWheel(flyWheelController);
   // User control code here, inside the loop
   while (1) {
     lMotor11.spin(fwd, Controller1.Axis2.position(percent), vex::velocityUnits::pct);
@@ -309,8 +191,8 @@ void usercontrol(void) {
     lMotor14.spin(fwd, Controller1.Axis2.position(percent), vex::velocityUnits::pct);
     rMotor20.spin(fwd, Controller1.Axis3.position(percent), vex::velocityUnits::pct);
 
-    Controller1.ButtonX.pressed(toggleFlyWheelOn);
-    Controller1.ButtonY.pressed(toggleFlyWheelOff);
+    //Controller1.ButtonX.pressed(toggleFlyWheelOn);
+    //Controller1.ButtonY.pressed(toggleFlyWheelOff);
 
     if (Controller1.ButtonR1.pressing()) {
       magLifter.set(true);
