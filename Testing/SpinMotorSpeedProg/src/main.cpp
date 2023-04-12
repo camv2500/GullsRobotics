b82517b2324fb25b79a76c33a7b2d0708fd691d7
Dylan Williams <dylan.williams.mmhs@gmail.com>
2023-04-12 12:44:19 -0400

    sppedprog

---
+++ Testing/SpinMotorSpeedProg/src/main.cpp
@@ -0,0 +1,68 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\User                                             */
+/*    Created:      Wed Apr 12 2023                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Motor1               motor         1               
+// Controller1          controller                    
+// Flywheel             motor         2               
+// Intake               motor         3               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+  if (Controller1.ButtonUp.pressing()) {
+    Motor1.spin(fwd,10,pct);
+  }
+  if (Controller1.ButtonRight.pressing()) {
+    Motor1.spin(fwd,20,pct);
+  }
+  if (Controller1.ButtonDown.pressing()) {
+    Motor1.spin(fwd,30,pct);
+  }
+  if (Controller1.ButtonLeft.pressing()) {
+    Motor1.spin(fwd,40,pct);
+  }
+  if (Controller1.ButtonX.pressing()) {
+    Motor1.spin(fwd,50,pct);
+  }
+  if (Controller1.ButtonA.pressing()) {
+    Motor1.spin(fwd,60,pct);
+  }
+  if (Controller1.ButtonB.pressing()) {
+    Motor1.spin(fwd,70,pct);
+  }
+  if (Controller1.ButtonY.pressing()) {
+    Motor1.spin(fwd,80,pct);
+  }
+  if (Controller1.ButtonL1.pressing()) {
+    Motor1.spin(fwd,90,pct);
+  }
+  if (Controller1.ButtonR1.pressing()) {
+    Motor1.spin(fwd,100,pct);
+  }
+
+  //flywheel
+  if (Controller1.ButtonL2.pressing()) {
+    Flywheel.spin(fwd,100,pct);
+  }
+
+  //intake
+  if (Controller1.ButtonR2.pressing()) {
+    Intake.spin(fwd,100,pct);
+  }
+  
+}
---
+++ Testing/SpinMotorSpeedProg/src/robot-config.cpp
@@ -0,0 +1,27 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor Motor1 = motor(PORT1, ratio18_1, false);
+controller Controller1 = controller(primary);
+motor Flywheel = motor(PORT2, ratio18_1, false);
+motor Intake = motor(PORT3, ratio18_1, false);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
