b17eb66d326f0c0d0a1b0dcf605ad2a87e418b91
Corey Phillips <cep.personal@protonmail.com>
2024-10-09 19:03:15 -0400

    PROS - C+++ameron

---
+++ HighStakes/moveBot/src/Autonomous.cpp
@@ -0,0 +1,19 @@
+#include "robot-config.h"
+#include "DriverControl.h"
+#include "AutonFunctions.h"
+#include "Autonomous.h"
+
+using namespace pros;
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
+
+void Autonomous() {}
