1b91bf49fc77931f924495fd4ea7be295ce4b532
RayP11 <Raypoulton11@yahoo.com>
2024-10-11 17:36:33 -0400

    fixed the polarity in DriverControl.cpp

--- HighStakes/RayProjects/moveBot/src/robot-config.cpp
+++ HighStakes/RayProjects/moveBot/src/robot-config.cpp
@@ -16,13 +16,13 @@ using namespace pros;
 
 // Motors and Sensors
 Motor back_left_wheels (BACK_LEFT_PORT, true);
-Motor back_right_wheels (BACK_RIGHT_PORT, false); 
-Motor middle_left_wheels (MIDDLE_LEFT_PORT, false);
+Motor back_right_wheels (BACK_RIGHT_PORT, true); 
+Motor middle_left_wheels (MIDDLE_LEFT_PORT, true);
 Motor middle_right_wheels (MIDDLE_RIGHT_PORT, true); 
 Motor front_left_wheels (FRONT_LEFT_PORT, true);
 Motor front_right_wheels (FRONT_RIGHT_PORT, false); 
-Motor top_front_right_wheels (TOP_FRONT_RIGHT_PORT, false);
-Motor top_front_left_wheels (TOP_FRONT_LEFT_PORT, true);
+Motor top_front_right_wheels (TOP_FRONT_RIGHT_PORT, true);
+Motor top_front_left_wheels (TOP_FRONT_LEFT_PORT, false);
 Motor top_back_left_wheels (TOP_BACK_LEFT_PORT, false);
 Motor top_back_right_wheels (TOP_BACK_RIGHT_PORT, true);
 
