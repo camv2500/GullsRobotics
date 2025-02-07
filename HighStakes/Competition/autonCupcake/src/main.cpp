170004c4f3b139a6fa00f97dcc33f43e47134400
C++ameron <camv2500@gmail.com>
2025-02-07 17:14:31 -0500

    Fixed the autonomous issue

--- HighStakes/Competition/autonCupcake/src/main.cpp
+++ HighStakes/Competition/autonCupcake/src/main.cpp
@@ -26,7 +26,9 @@ void disabled() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+void autonomous() {
+	Autonomous();
+}
 
 
 /**
